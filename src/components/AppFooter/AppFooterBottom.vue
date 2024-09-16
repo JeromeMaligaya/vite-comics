e1e7ceac47f4289c6a5bb7326c0efdc566342370
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-17 00:36:04 +0200

    added iconList[] on AppFooterBottom

--- src/components/AppFooter/AppFooterBottom.vue
+++ src/components/AppFooter/AppFooterBottom.vue
@@ -2,7 +2,13 @@
 export default {
   data(){
     return{
-
+        iconList: [
+            {image:'footer-facebook.png'},
+            {image:'footer-periscospe.png'},
+            {image:'footer-pinterest.png'},
+            {image:'footer-twitter.png'},
+            {image:'footer-youtube.png'},
+        ]
     }
   },
   components: {
