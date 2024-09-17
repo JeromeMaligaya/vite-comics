e763fbbe62a0b4dcc48a9d91aa49eacfc3a52d35
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-17 23:59:15 +0200

    added badge current series on AppMainJumbotron

--- src/components/AppMain/AppMainJumbotron.vue
+++ src/components/AppMain/AppMainJumbotron.vue
@@ -15,15 +15,30 @@ export default {
 </script>
 
 <template>
-    <div class="jumbotron"></div>
+    <div class="jumbotron">
+        <div class="container">
+        <p class="current-series-badge">CURRENT SERIES</p>
+    </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+
     .jumbotron{
         background-image: url(../../assets/img/jumbotron.jpg);
         background-position: top;
         background-size: cover;
         height: 300px;
+        
+        .current-series-badge{
+            color: white;
+            background-color: rgb(2, 130, 249);
+            font-size: 15px;
+            display: inline-block;
+            padding: 5px 15px;
+            position: relative;
+            top:288px;
+        }
     }
 
 </style>
