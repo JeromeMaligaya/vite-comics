638a3cf7397c2992fa2d8219ca5f4c8eccece08c
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-17 23:28:40 +0200

    added template and style on AppMainCard

--- src/components/AppMain/AppMainListCards/AppMainCard.vue
+++ src/components/AppMain/AppMainListCards/AppMainCard.vue
@@ -34,9 +34,31 @@ export default {
 </script>
 
 <template>
-
+    <li class="card">
+        <a href="#">
+            <div class="img-box">
+                <img :src="thumbUrl" :alt="thumbUrl" />
+            </div>
+        </a>
+        <a href="#"><h3>{{ series }}</h3></a>
+    </li>
 </template>
 
 <style lang="scss" scoped>
+    @use "../../../style/general.scss" as *;
+    
+    .card {
+        flex-basis: calc(100% / 6 - 90px / 6);
+
+        .img-box{
+            height: 150px;
+            overflow: hidden;
+        }
+        h3{
+            color: white;
+            font-size: 10px;
+            margin-top: 15px;
+        }
+    }
 
 </style>
