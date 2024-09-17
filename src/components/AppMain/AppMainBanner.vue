845815b0b3d511e0d7f518058ea50dc8ec92d800
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-17 17:18:26 +0200

    completed style on AppMainBanner

--- src/components/AppMain/AppMainBanner.vue
+++ src/components/AppMain/AppMainBanner.vue
@@ -34,4 +34,26 @@ export default {
 
 <style lang="scss" scoped>
 
+    .banner-list{
+        display: flex;
+        justify-content: center;
+        gap: 30px;
+        padding: 30px 0;
+
+        li{
+            display: flex;
+            align-items: center;
+            gap: 10px;
+
+            a{
+                width: 30px;
+            }
+
+            span{
+                color: white;
+                font-size: 12px;
+            }
+        }
+    }
+
 </style>
