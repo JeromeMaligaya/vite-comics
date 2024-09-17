1e74b034c05a24e0132e6fa7791e72ac9d767668
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-17 16:48:27 +0200

    added iconBannerList on AppMainBanner

--- src/components/AppMain/AppMainBanner.vue
+++ src/components/AppMain/AppMainBanner.vue
@@ -2,7 +2,13 @@
 export default {
   data(){
     return{
-        
+        iconBannerList: [
+            {iconUrl: 'buy-comics-digital-comics.png', label: 'digital comics'},
+            {iconUrl: 'buy-comics-merchandise.png', label: 'dc merchandise'},
+            {iconUrl: 'buy-comics-subscriptions.png', label: 'subscription'},
+            {iconUrl: 'buy-comics-shop-locator.png', label: 'comic shop locator'},
+            {iconUrl: 'buy-dc-power-visa.png', label: 'dc power visa'},
+        ]
     }
   },
   components: {
