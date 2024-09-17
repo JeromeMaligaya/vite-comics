d058b8ee4319dfa697fcc11681e50ccc279400f8
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-09-17 23:41:32 +0200

    added template and style on AppMainJumbotron

--- src/components/AppMain/AppMainJumbotron.vue
+++ src/components/AppMain/AppMainJumbotron.vue
@@ -15,10 +15,15 @@ export default {
 </script>
 
 <template>
-
+    <div class="jumbotron"></div>
 </template>
 
 <style lang="scss" scoped>
-   
+    .jumbotron{
+        background-image: url(../../assets/img/jumbotron.jpg);
+        background-position: top;
+        background-size: cover;
+        height: 300px;
+    }
 
 </style>
